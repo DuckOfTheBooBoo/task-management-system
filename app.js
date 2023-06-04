750ee80bd5563e5a286dc4c2f3f5e50481e03687
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-06-04 14:34:23 +0700

    Remove cookie parser because unused

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.set('view engine', 'ejs');
 app.use(logActivity);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
 app.use('/', indexRoute);
 app.use('/api', apiRoute);
 
