502f7efe74f0f8ec17e374e9ccb353ef78f288b9
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-06-04 16:34:45 +0700

    Remove unused property

--- models/task.model.js
+++ models/task.model.js
@@ -8,22 +8,9 @@ const Task = sequelize.define('Task', {
     primaryKey: true,
     allowNull: false,
   },
-  user_id: {
-    type: DataTypes.UUID,
-    allowNull: false,
-  },
   task_desc: {
     type: DataTypes.STRING,
     allowNull: false,
-  },
-  date_created: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-    timezone: false,
-  },
-  date_updated: {
-    type: DataTypes.DATE,
   },
   status: {
     type: DataTypes.STRING,
