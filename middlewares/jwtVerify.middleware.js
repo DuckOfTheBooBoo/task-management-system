59f78cb4ff1e6ef3de7f53e9689f882fc9391f6b
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-06-07 17:35:29 +0700

    Add jwt verification middleware

---
+++ middlewares/jwtVerify.middleware.js
@@ -0,0 +1,49 @@
+/* eslint-disable valid-jsdoc */
+const jwt = require('jsonwebtoken');
+const RevokedToken = require('../models/revokedTokens.model');
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+const verifyJwt = async (req, res, next) => {
+  const jwtToken = req.jwtToken;
+
+  try {
+    // Check if the token is revoked
+    const result = await RevokedToken.findOne({
+      where: {
+        token: jwtToken,
+      },
+    });
+
+    if (result) {
+      return res.status(400).json({
+        status: 'fail',
+        message: 'Token is revoked.',
+      });
+    }
+
+    // Verify token
+    try {
+      const decodedToken = jwt.verify(jwtToken, process.env.JWT_SECRET);
+      req.userId = decodedToken.userId;
+      return next();
+
+    } catch (err) {
+      return res.status(401).json({
+        status: 'fail',
+        message: 'Invalid or expired token.',
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      status: 'fail',
+      message: 'Server error',
+    });
+  }
+};
+
+module.exports = verifyJwt;
