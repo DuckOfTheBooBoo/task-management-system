2e5a2a01a15a5c43c8cdea57faebf13b13b5af53
Arajdian Altaf <arajdianaltaf123@gmail.com>
2023-06-13 11:29:56 +0700

    Use req.jwtToken for token container

--- controllers/logout.controller.js
+++ controllers/logout.controller.js
@@ -5,35 +5,31 @@ const RevokedToken = require('../models/revokedTokens.model');
 const addMinute = require('../utils/addMinute');
 
 const logoutUser = async (req, res) => {
-  const authHeader = req.headers.authorization;
+  const token = req.jwtToken;
 
-  if (authHeader && authHeader.startsWith('Bearer')) {
-    const token = authHeader.split(' ')[1];
+  if (!token) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'No JWT token provided.',
+    });
+  }
 
-    if (!token) {
-      return res.status(400).json({
-        status: 'fail',
-        message: 'No JWT token provided.',
-      });
-    }
+  try {
+    await RevokedToken.create({
+      token: token,
+    });
 
-    try {
-      await RevokedToken.create({
-        token: token,
-      });
+    return res.json({
+      status: 'success',
+      message: 'Successfully logged out',
+    });
+  } catch (err) {
+    console.error(err);
 
-      return res.json({
-        status: 'success',
-        message: 'Successfully logged out',
-      });
-    } catch (err) {
-      console.error(err);
-
-      return res.status(500).json({
-        status: 'fail',
-        message: 'Server error',
-      });
-    }
+    return res.status(500).json({
+      status: 'fail',
+      message: 'Server error',
+    });
   }
 };
 
